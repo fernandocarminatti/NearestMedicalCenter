e6ba5377d3f01c35a9be2068e735a4dbc04315af
Fernando Carminatti <nandocarminatti@hotmail.com>
2024-07-17 14:52:41 -0300

    First Commit

---
+++ src/main/java/com/nearestmedicalcenter/NearestMedicalCenter/NearestMedicalCenterApplication.java
@@ -0,0 +1,13 @@
+package com.nearestmedicalcenter.NearestMedicalCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearestMedicalCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearestMedicalCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nearestmedicalcenter/NearestMedicalCenter/NearestMedicalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nearestmedicalcenter.NearestMedicalCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearestMedicalCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
